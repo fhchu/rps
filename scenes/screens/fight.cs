847e178394ab8856cde041dfac6c86e5e0789a55
fhchu <felixhchu@gmail.com>
2023-04-08 12:13:30 -0700

    figuring out powerup inheritence

--- scenes/screens/fight.cs
+++ scenes/screens/fight.cs
@@ -7,7 +7,7 @@ public partial class fight : Control
     //Allows us to access and update UI elements from this file
     const String PLAYER_NODE_PATH = "LocalHBox/PlayerVBox";
     const String GAMELOG_PATH = "CanvasLayer/TextboxMargin/GameLog";
-    const String UPGRADES_PATH = "CanvasLayer/UpgradesHbox/GameLog";
+    const String UPGRADES_PATH = "CanvasLayer/UpgradesHBox";
 
     const String HANDS_UI_PATH = "/AttacksPanel/HandsHBox";
     const int NUM_PLAYERS = 2;
@@ -15,12 +15,13 @@ public partial class fight : Control
 
     private Player[] players;
     private List<RoundResult> roundResults;
-    // this is an array because we might want more rounds in the future
+    // this is an array because we might want more phases in the future
     private int[] phaseHealth = { 20 };
     private int totalPhases = 1;
     private int currentPhase;
     private bool isCombatEnabled;
     private bool isUpgradePhase;
+    private int upgradingPlayer;
 
     Panel gameLog;
 
@@ -77,7 +78,38 @@ public partial class fight : Control
                 throwHands();
             }
         }
-        else if (isUpgradePhase){
+        else if (isUpgradePhase)
+        {
+            if (upgradingPlayer == 0)
+            {
+                if (Input.IsActionJustPressed("rock0"))
+                {
+                    GD.Print("1");
+                }
+                if (Input.IsActionJustPressed("paper0"))
+                {
+                    GD.Print("2");
+                }
+                if (Input.IsActionJustPressed("scissors0"))
+                {
+                    GD.Print("3");
+                }
+            }
+            else
+            {
+                if (Input.IsActionJustPressed("rock1"))
+                {
+                    GD.Print("1");
+                }
+                if (Input.IsActionJustPressed("paper1"))
+                {
+                    GD.Print("2");
+                }
+                if (Input.IsActionJustPressed("scissors1"))
+                {
+                    GD.Print("3");
+                }
+            }
 
         }
     }
@@ -89,7 +121,7 @@ public partial class fight : Control
         players = new Player[NUM_PLAYERS];
         for (int i = 0; i < NUM_PLAYERS; i++)
         {
-            Player player = new Player(PLAYER_NODE_PATH + i);
+            Player player = new Player(PLAYER_NODE_PATH + i, i);
 
             players[i] = player;
             // player does not handle setting of their own powers
@@ -153,15 +185,20 @@ public partial class fight : Control
         {
 
         }
+        roundResults.Add(new RoundResult());
         winner.calculateHandValues();
         loser.calculateHandValues();
     }
 
-    private void selectUpgrade(Player player, int selection){
-        isCombatEnabled = false;
-        isUpgradePhase = true;
-        player.powerUpsObtained.Add(powerUps)
+    private void selectUpgrade(Player player, int selection)
+    {
+        Control upgrades = (Control)GetNode(UPGRADES_PATH);
+        isUpgradePhase = false;
+        isCombatEnabled = true;
+
+        upgrades.Hide();
     }
+
     private void endPhase(Player loser)
     {
         if (currentPhase == totalPhases)
@@ -173,7 +210,6 @@ public partial class fight : Control
             isCombatEnabled = false;
         }
     }
-
 
     //presumably the player knows their own max health. we are simply setting the health to this value.
     // returns true or false based on ?? TODO
@@ -190,12 +226,27 @@ public partial class fight : Control
         {
             endPhase(player);
         }
-        else if (player.upgradeThresholds.Count > 0 && player.currentHealth <= player.maxHealth - player.upgradeThresholds[0]){
-            isUpgradePhase = true;
-            isCombatEnabled = false;
+        else if (player.upgradeThresholds.Count > 0 && player.currentHealth <= player.maxHealth - player.upgradeThresholds[0])
+        {
+            startUpgradePhase(player);
         }
 
         return false;
+    }
+
+
+    private void startUpgradePhase(Player player)
+    {
+        isUpgradePhase = true;
+        isCombatEnabled = false;
+        upgradingPlayer = player.playerId;
+        Control upgrades = (Control)GetNode(UPGRADES_PATH);
+        for (int i = 0; i < 3; i++)
+        {
+            Control marginContainer = (Control)upgrades.GetNode("MarginContainer" + i + "/Panel/VBoxContainer");
+
+        }
+        upgrades.Show();
     }
 
     //updates the player's hand power in backend and UI
@@ -206,7 +257,6 @@ public partial class fight : Control
         Button handButton = (Button)GetNode(player.VBoxPath + HANDS_UI_PATH + "/" + handName + "Button");
         handButton.Text = handName + ": " + value;
     }
-
 
     //update all 3 hands at once
     private void setAllHands(Player player, int rockValue, int paperValue, int scissorsValue)
@@ -225,18 +275,21 @@ public partial class fight : Control
     // hold the player data. player object does NOT handle their own UI
     public class Player
     {
+        public int playerId;
         public String VBoxPath;
         public int maxHealth;
         public int currentHealth;
         //these are white (unconditional) hand values
+        // rock = 0, paper = 1, scissors = 2
         public int[] baseHandValues;
         public PowerUp[] powerUpLibrary;
         public List<int> upgradeThresholds;
         public List<PowerUp> powerUpsObtained;
         public Hand thrownHand;
         public bool hasThrown;
-        public Player(String path)
+        public Player(String path, int id)
         {
+            playerId = id;
             VBoxPath = path;
             baseHandValues = new int[3];
             hasThrown = false;
@@ -262,14 +315,28 @@ public partial class fight : Control
         //we're saving damage for the UI even if it's not used for mechanics
         // this is an array because each player can take different amounts of damage in one round
         public int[] damage;
+
+        RoundResult(Hand player0Hand, Hand player1Hand, )
     }
-    public class PowerUp
+
+    public abstract class PowerUp
     {
         public String title;
         public String description;
-        public int[] calculateDamage()
+        public abstract int[] calculateDamage(Player player);
+    }
+
+    public class BigRock : PowerUp
+    {
+        BigRock()
         {
-            return new int[3];
+            title = "Big Rock";
+            description = "Every third Rock you throw breaks ties";
+        }
+        public override int[] calculateDamage(Player player)
+        {
+            if ()
+
         }
     }
 }
